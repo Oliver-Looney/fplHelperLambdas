3bd86ac71523388cb803151fecf4797586258f61
Oliver Looney <oliverjlooney@gmail.com>
2022-08-27 23:04:52 +0100

    lambda & sns configed

--- gameweekDeadlineReminders/main.go
+++ gameweekDeadlineReminders/main.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sns"
+	"os"
+	"strconv"
 	"time"
 )
 
@@ -20,23 +27,58 @@ func getFPLGameweekData() (*fplBootstrapResponse, error) {
 	return fplBootstrapResponse, nil
 }
 
-func main() {
+func generateSMSContents(gameweek gameweekData) string {
+	return gameweek.Name + " Deadline is in " + strconv.FormatInt((gameweek.DeadlineTimeEpoch-time.Now().Unix())/86400, 10) + " days and " + strconv.FormatInt((gameweek.DeadlineTimeEpoch-time.Now().Unix()%86400)/3600, 10) + " hours"
+}
+
+type MyEvent struct {
+	Name string `json:"name"`
+}
+
+func HandleRequest(ctx context.Context, name MyEvent) {
 	fplGameweekData, err := getFPLGameweekData()
 	if err != nil {
 		println("ERROR")
 	}
-	for i := 0; i < len(fplGameweekData.Events); i++ {
-		fmt.Println(fplGameweekData.Events[i])
+	i := 0
+	for !fplGameweekData.Events[i].IsNext {
+		i++
 	}
+	sess := session.Must(session.NewSession())
+
+	svc := sns.New(sess)
+
+	params := &sns.PublishInput{
+		Message:     aws.String(generateSMSContents(fplGameweekData.Events[i])),
+		PhoneNumber: aws.String(os.Getenv("MY_PHONE_NUMBER")),
+	}
+	resp, err := svc.Publish(params)
+
+	if err != nil {
+		// Print the error, cast err to awserr.Error to get the Code and
+		// Message from an error.
+		fmt.Println(err.Error())
+		return
+	}
+
+	// Pretty-print the response data.
+	fmt.Println(resp)
 	return
 }
 
+func main() {
+	lambda.Start(HandleRequest)
+}
+
+type gameweekData struct {
+	ID                int       `json:"id"`
+	Name              string    `json:"name"`
+	DeadlineTime      time.Time `json:"deadline_time"`
+	Finished          bool      `json:"finished"`
+	IsNext            bool      `json:"is_next"`
+	DeadlineTimeEpoch int64     `json:"deadline_time_epoch"`
+}
+
 type fplBootstrapResponse struct {
-	Events []struct {
-		ID                int       `json:"id"`
-		Name              string    `json:"name"`
-		DeadlineTime      time.Time `json:"deadline_time"`
-		Finished          bool      `json:"finished"`
-		DeadlineTimeEpoch int       `json:"deadline_time_epoch"`
-	} `json:"events"`
+	Events []gameweekData `json:"events"`
 }
